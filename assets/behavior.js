4c3e3cdb686fe2e67becd684e678b6d2053bb288
aureooms <aurelien.ooms@gmail.com>
2015-09-13 00:40:40 +0200

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -151,6 +151,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "Use",
+            "slug": "use"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "Optimization of the code step by step",
             "slug": "optimization-of-the-code-step-by-step"
           },
