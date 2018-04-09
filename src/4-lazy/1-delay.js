d39f568c7c293ca5c7afe380f6dfb55b4e526b84
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-04-09 17:56:09 +0200

    typo

--- src/4-lazy/1-delay.js
+++ src/4-lazy/1-delay.js
@@ -4,7 +4,7 @@ import { Lazy } from '.' ;
  * Wraps a tree-returning function with a tree proxy that will shallow evaluate
  * as soon as a method is called on it.
  *
- * @param Function thunk The function that evaluates to the underlying tree.
+ * @param {Function} thunk The function that evaluates to the underlying tree.
  * @returns {Tree} The lazy tree.
  */
 export function delay ( thunk ) {
