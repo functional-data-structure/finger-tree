fcd68c7daf89276a5496ba53d9faecd520b2635d
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-12-23 18:07:09 +0100

    :rocket: perf(_from_digit): Replace throw by optional assert.

--- src/0-core/_fast/_from_digit.js
+++ src/0-core/_fast/_from_digit.js
@@ -2,18 +2,12 @@ import {One, Two, Three, Four} from '../../1-digit';
 import {Empty, Single, Deep} from '../../3-tree';
 import {cache} from '../measure';
 
+import assert from 'assert';
+
 export function _from_digit(M, digit) {
 	if (digit instanceof One) return new Single(M, digit.a);
-	if (digit instanceof Two || digit instanceof Three || digit instanceof Four) {
-		return new Deep(
-			M,
-			digit.init(),
-			new Empty(cache(M)),
-			new One(digit.last())
-		);
-	}
-
-	// Potential optimization by commenting out this section
-	// and remove the second test above
-	throw new Error('second argument is not a Digit');
+	assert(
+		digit instanceof Two || digit instanceof Three || digit instanceof Four
+	);
+	return new Deep(M, digit.init(), new Empty(cache(M)), new One(digit.last()));
 }
