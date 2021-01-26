9e49f0882a5e670f272cef4e81a5ac6e187e99d0
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-26 18:41:35 +0100

    :mag: test: Cover Lazy#split.
    Fixes #121.

---
+++ test/src/github-issue-121.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+
+import {Measures} from '@aureooms/js-measure';
+const {COUNTER} = Measures;
+
+import {gt} from '@aureooms/js-predicate';
+
+import {from} from '../../src';
+
+test('Cover Lazy#split', (t) => {
+	const T = from(COUNTER, 'abcde');
+	const split = T.splitTree(gt(2), COUNTER.zero());
+	t.is([...split.left.takeUntil(gt(1))].join(''), 'a');
+});
