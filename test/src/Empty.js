6d55c46d0e2186484cfa9b8710f019f020335313
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-01-26 18:43:48 +0100

    :mag: test: Cover throwing Empty methods.
    Covers Empty#head, Empty#last, and Empty#splitTree.

---
+++ test/src/Empty.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+
+import {Measures} from '@aureooms/js-measure';
+const {COUNTER} = Measures;
+
+import {gt} from '@aureooms/js-predicate';
+
+import {empty} from '../../src';
+
+test('head', (t) => {
+	t.throws(() => empty(COUNTER).head(), {message: /cannot/});
+});
+
+test('last', (t) => {
+	t.throws(() => empty(COUNTER).last(), {message: /cannot/});
+});
+
+test('splitTree', (t) => {
+	t.throws(() => empty(COUNTER).splitTree(gt(0), COUNTER.zero()), {
+		message: /not implemented/
+	});
+});
