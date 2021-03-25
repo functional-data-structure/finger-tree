6873d2e4a52c5020d0cb1229a5a7f9e084fb3819
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-03-25 16:09:06 +0100

    :robot: docs: Simplify scripts.
    Replace querySelectorAll(...)[0] by querySelector(...) in docs scripts.
    
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/aureooms/rejuvenate/blob/e086830dfe927964be93f46b8ecdacd2597eb487/src/transforms/codemod:doc-scripts-header-replace-querySelectorAll.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -13,7 +13,7 @@ domReady(() => {
 	projectname.text = 'aureooms/js-fingertree';
 	projectname.href = './index.html';
 
-	const header = document.querySelectorAll('header')[0];
+	const header = document.querySelector('header');
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
