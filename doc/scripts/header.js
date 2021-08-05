26f58d9e785d3480ce74440bf43926fa30d16e3b
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-08-05 10:28:47 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/a44c42967442d65fd14272682fb84a6f32feb841/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -18,7 +18,7 @@ domReady(() => {
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
 	testlink.href =
-		'https://coveralls.io/github/functional-data-structure/finger-tree';
+		'https://app.codecov.io/gh/functional-data-structure/finger-tree';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
