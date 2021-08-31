0646b311c959c1be7923142de4c6c4355f5d852a
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-08-31 21:22:52 +0200

    :bicyclist: perf: Inline loopy code inside core/concatenate/nodes.

--- src/0-core/concatenate/nodes.js
+++ src/0-core/concatenate/nodes.js
@@ -4,29 +4,57 @@ import {node2, node3} from '../../2-node/index.js';
 
 export function nodes(M, list) {
 	assert(list.length >= 3 && list.length <= 12);
-	const out = [];
-
-	const n = list.length;
-
-	let i = 0;
-
-	switch (n % 3) {
-		case 1:
-			out.push(node2(M, list[0], list[1]));
-			out.push(node2(M, list[2], list[3]));
-			i += 4;
-			break;
-		case 2:
-			out.push(node2(M, list[0], list[1]));
-			i += 2;
-			break;
+	switch (list.length) {
+		case 3:
+			return [node3(M, list[0], list[1], list[2])];
+		case 4:
+			return [node2(M, list[0], list[1]), node2(M, list[2], list[3])];
+		case 5:
+			return [node3(M, list[0], list[1], list[2]), node2(M, list[3], list[4])];
+		case 6:
+			return [
+				node3(M, list[0], list[1], list[2]),
+				node3(M, list[3], list[4], list[5]),
+			];
+		case 7:
+			return [
+				node2(M, list[0], list[1]),
+				node3(M, list[2], list[3], list[4]),
+				node2(M, list[5], list[6]),
+			];
+		case 8:
+			return [
+				node3(M, list[0], list[1], list[2]),
+				node2(M, list[3], list[4]),
+				node3(M, list[5], list[6], list[7]),
+			];
+		case 9:
+			return [
+				node3(M, list[0], list[1], list[2]),
+				node3(M, list[3], list[4], list[5]),
+				node3(M, list[6], list[7], list[8]),
+			];
+		case 10:
+			return [
+				node2(M, list[0], list[1]),
+				node3(M, list[2], list[3], list[4]),
+				node3(M, list[5], list[6], list[7]),
+				node2(M, list[8], list[9]),
+			];
+		case 11:
+			return [
+				node2(M, list[0], list[1]),
+				node3(M, list[2], list[3], list[4]),
+				node3(M, list[5], list[6], list[7]),
+				node3(M, list[8], list[9], list[10]),
+			];
 		default:
-			break;
+			assert(list.length === 12);
+			return [
+				node3(M, list[0], list[1], list[2]),
+				node3(M, list[3], list[4], list[5]),
+				node3(M, list[6], list[7], list[8]),
+				node3(M, list[9], list[10], list[11]),
+			];
 	}
-
-	for (; i < n; i += 3) {
-		out.push(node3(M, list[i], list[i + 1], list[i + 2]));
-	}
-
-	return out;
 }
