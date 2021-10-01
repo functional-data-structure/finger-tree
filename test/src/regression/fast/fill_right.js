043c60286d4e9f3321aa39478c4dbc2baa99393e
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-01 11:45:45 +0200

    :umbrella: test(append): Prevent regression on append's pureness.

--- test/src/regression/fast/fill_right.js
+++ test/src/regression/fast/fill_right.js
@@ -7,12 +7,13 @@ import {ABSTRACT_COUNTER as COUNTER, measure} from '../../_fixtures.js';
 import {empty} from '../../../../src/index.js';
 
 test('left instanceof Four', (t) => {
-	const tree = empty(COUNTER)
-		.append(range(2))
-		.cons(-1)
-		.cons(-2)
-		.cons(-3)
-		.append(range(2, 4));
-	t.deepEqual(Array.from(tree), Array.from(range(-3, 4)));
-	t.is(tree.measure(), measure(COUNTER, range(-3, 4)));
+	const before = empty(COUNTER).append(range(2)).cons(-1).cons(-2).cons(-3);
+
+	const after = before.append(range(2, 4));
+
+	t.deepEqual(Array.from(after), Array.from(range(-3, 4)));
+	t.is(after.measure(), measure(COUNTER, range(-3, 4)));
+
+	t.deepEqual(Array.from(before), Array.from(range(-3, 2)));
+	t.is(before.measure(), measure(COUNTER, range(-3, 2)));
 });
--- test/src/regression/tree/implementations/Deep/append.js
+++ test/src/regression/tree/implementations/Deep/append.js
@@ -9,25 +9,37 @@ import {from} from '../../../../../../src/index.js';
 test('empty', (t) => {
 	const r0 = range(0);
 	const r200 = range(200);
-	let tree = from(COUNTER, r200);
-	tree = tree.append(r0);
-	t.deepEqual(Array.from(tree), Array.from(r200).concat(Array.from(r0)));
-	t.is(tree.measure(), 200);
+	const before = from(COUNTER, r200);
+	const after = before.append(r0);
+
+	t.deepEqual(Array.from(after), Array.from(r200).concat(Array.from(r0)));
+	t.is(after.measure(), 200);
+
+	t.deepEqual(Array.from(before), Array.from(r200));
+	t.is(before.measure(), 200);
 });
 
 test('range(1)', (t) => {
 	const r1 = range(1);
 	const r200 = range(200);
-	let tree = from(COUNTER, r200);
-	tree = tree.append(r1);
-	t.deepEqual(Array.from(tree), Array.from(r200).concat(Array.from(r1)));
-	t.is(tree.measure(), 201);
+	const before = from(COUNTER, r200);
+	const after = before.append(r1);
+
+	t.deepEqual(Array.from(after), Array.from(r200).concat(Array.from(r1)));
+	t.is(after.measure(), 201);
+
+	t.deepEqual(Array.from(before), Array.from(r200));
+	t.is(before.measure(), 200);
 });
 
 test('r200', (t) => {
 	const r200 = range(200);
-	let tree = from(COUNTER, r200);
-	tree = tree.append(r200);
-	t.deepEqual(Array.from(tree), Array.from(r200).concat(Array.from(r200)));
-	t.is(tree.measure(), 400);
+	const before = from(COUNTER, r200);
+	const after = before.append(r200);
+
+	t.deepEqual(Array.from(after), Array.from(r200).concat(Array.from(r200)));
+	t.is(after.measure(), 400);
+
+	t.deepEqual(Array.from(before), Array.from(r200));
+	t.is(before.measure(), 200);
 });
