d01ee6a69b590908cd10f3176dec462804d0cd3d
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-10-14 14:51:16 +0200

    :broom: cleaning(Node2,Node3): Remove dead code.

--- src/2-node/2-Node2.js
+++ src/2-node/2-Node2.js
@@ -21,27 +21,3 @@ Node2.prototype.measure = function () {
 Node2.prototype.digit = function () {
 	return new Two(this.a, this.b);
 };
-
-Node2.prototype.head = function () {
-	throw new Error('trying to call head of Node2');
-};
-
-Node2.prototype.last = function () {
-	throw new Error('trying to call last of Node2');
-};
-
-Node2.prototype.init = function () {
-	throw new Error('trying to call init of Node2');
-};
-
-Node2.prototype.tail = function () {
-	throw new Error('trying to call tail of Node2');
-};
-
-Node2.prototype.push = function (_value) {
-	throw new Error('trying to call push of Node2');
-};
-
-Node2.prototype.cons = function (_value) {
-	throw new Error('trying to call cons of Node2');
-};
--- src/2-node/3-Node3.js
+++ src/2-node/3-Node3.js
@@ -22,27 +22,3 @@ Node3.prototype.measure = function () {
 Node3.prototype.digit = function () {
 	return new Three(this.a, this.b, this.c);
 };
-
-Node3.prototype.head = function () {
-	throw new Error('trying to call head of Node3');
-};
-
-Node3.prototype.last = function () {
-	throw new Error('trying to call last of Node3');
-};
-
-Node3.prototype.init = function () {
-	throw new Error('trying to call init of Node3');
-};
-
-Node3.prototype.tail = function () {
-	throw new Error('trying to call tail of Node3');
-};
-
-Node3.prototype.push = function (_value) {
-	throw new Error('trying to call push of Node3');
-};
-
-Node3.prototype.cons = function (_value) {
-	throw new Error('trying to call cons of Node3');
-};
